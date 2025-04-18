17e576fa758555502e4f61603c550b62f944b77a
charanrajb28 <charanrajb282004@gmail.com>
2025-04-18 12:19:29 +0530

    second commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,16 @@
+import Sidebar from "@/components/ui/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggin={firstName:'Charan',lastName:'Raj B'}
   return (
     <main>
-        SIDEBAR
+        <Sidebar user={loggin}/>
         {children}
     </main>
+    
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName: 'Charan'}
   return (
-    <div>
-      HOME
-    </div>
+    <section className='home'>
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type="greeting"
+                    title="welcome"
+                    user={loggedIn?.firstName|| 'guest'}
+                    subtext="Access and manage your account and transactions efficiently"
+                />
+
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
